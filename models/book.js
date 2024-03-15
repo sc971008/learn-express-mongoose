df88e6e0713dd6eaf77d102f46d28218da06ea18
Cheng Shi <sc971008@gmail.com>
2024-03-15 14:26:39 -0400

    fix schema

--- models/book.js
+++ models/book.js
@@ -3,7 +3,13 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var BookSchema = new Schema(
-  {}
+  {
+    title: {type:String, require:true,maxLength: 100},
+    author: {type: Schema.Types.ObjectId, ref: 'Author', required: true},
+    summary: {type: String},
+    isbn: {type: String},
+    genre: [{type: Schema.Types.ObjectId, ref: 'Genre'}]
+  }
 );
 
 //Export model
